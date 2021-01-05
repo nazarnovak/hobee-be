7c2cd0464bbbf2e6478fa755f639138733effca2
Nazar Novak <nazarnovak@gmail.com>
2021-01-05 10:39:18 +0100

    Feedbacks sql fix

--- db/feedbacks.sql
+++ db/feedbacks.sql
@@ -1,6 +1,6 @@
 CREATE TABLE feedbacks(
 	id bigserial PRIMARY KEY,
 	message text NOT NULL,
-	useruuid uuid NOT NULL
-	created timestamp without time zone NOT NULL,
+	useruuid uuid NOT NULL,
+	created timestamp without time zone NOT NULL
 );
